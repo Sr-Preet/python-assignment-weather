497a28b71e580e1a297e085414b1837ed604578b
Sarpreet <ssarpreet.786@gmail.com>
2023-11-24 13:04:48 -0500

    Updated gitignore to ignore env folder Enhanced app.py To Have Error Handling Functionality Added Comments To app.py

--- app.py
+++ app.py
@@ -2,7 +2,58 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+def get_weather_data(city, units='metric'):
+    # Set the URL for the OpenWeatherMap API
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
+    # Create a dictionary to store the parameters for the API request
+    params = {
+        'q': city,
+        'appid': 'a771e288754956cacb9456ec050c7e94',
+        'units': units
+    }
+
+    try:
+        # Make a GET request to the API using the requests library
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        # Convert the JSON response to a Python dictionary
+        data = response.json()
+
+        # Extract the required data from the dictionary
+        city = data['name']
+        temperature = data['main']['temp']
+        description = data['weather'][0]['description']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+
+        # Get the current date and day
+        now = datetime.now()
+        current_date = now.strftime("%d %b %Y")
+        current_day = now.strftime("%A")
+
+        return {
+            'city': city,
+            'temperature': temperature,
+            'description': description,
+            'humidity': humidity,
+            'wind_speed': wind_speed,
+            'current_date': current_date,
+            'current_day': current_day
+        }
+
+    except requests.exceptions.HTTPError as err:
+        print(f"Error fetching data from OpenWeatherMap API: {err}", 'error')
+        return None
+    except requests.exceptions.RequestException as err:
+        print(f"Error connecting to OpenWeatherMap API: {err}", 'error')
+        return None
+    except KeyError as err:
+        print(f"Error parsing OpenWeatherMap API response: {err}", 'error')
+        return None
 
 @app.route('/')
 def homepage():
@@ -10,27 +61,16 @@ def homepage():
 
 @app.route("/weatherapp", methods=["POST", "GET"])
 def get_weatherdata():
-    url="https://api.openweathermap.org/data/2.5/weather"
+    if request.method == 'POST':
+        city = request.form.get("city")
+        units = request.form.get("units", "metric")  # Default to metric if not provided
 
-    params ={
-        'q':request.form.get("city"),
-        'appid':'a771e288754956cacb9456ec050c7e94',
-        'units':'metric'
-    }
-    response=requests.get(url, params=params)
-    data = response.json()
-    city = data['name']
-    temperature = data['main']['temp']
-    description = data['weather'][0]['description']
-    humidity = data['main']['humidity']
-    wind_speed = data['wind']['speed']
+        weather_data = get_weather_data(city, units)
 
-    now = datetime.now()
-    current_date = now.strftime("%d %b %Y")
-    current_day = now.strftime("%A")
+        if weather_data:
+            return render_template('response.html', **weather_data)
 
+    return render_template('response.html', city=None, temperature=None, description=None, humidity=None, wind_speed=None, current_date=None, current_day=None)
 
-    return render_template('response.html', city=city, temperature=temperature, description=description, humidity=humidity, wind_speed=wind_speed, current_date=current_date, current_day=current_day)
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
